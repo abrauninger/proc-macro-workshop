ed4e41b4acab547f1a64788c80ff3c2696c0bed8
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2023-09-21 23:54:47 -0700

    Slight tweaks

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -22,7 +22,7 @@ fn custom_format_from_debug_attribute(attrs: &Vec<Attribute>) -> syn::Result<Opt
             if let Expr::Lit(ExprLit { lit: Lit::Str(lit_str), .. }) = value {
                 Ok(Some(lit_str.value()))
             } else {
-                Err(syn::Error::new_spanned(attr.meta.clone(), "expected `debug = \"...\"`"))
+                Err(syn::Error::new_spanned(&attr.meta, "expected `debug = \"...\"`"))
             }
         },
         _ => Ok(None),
@@ -33,50 +33,51 @@ fn custom_format_from_debug_attribute(attrs: &Vec<Attribute>) -> syn::Result<Opt
 pub fn derive(input: TokenStream) -> TokenStream {
     let derive_input = parse_macro_input!(input as DeriveInput);
 
-    if let DeriveInput {
-        ident: struct_name,
-        data: Data::Struct(
-            DataStruct {
-                fields: Fields::Named(FieldsNamed { named: fields, .. }), ..
-            }
-        ), ..
-    } = derive_input {
-        let debug_struct_fields: proc_macro2::TokenStream = fields.iter().map(|field| {
-            if let Field { ident: Some(field_name), attrs, .. } = &field {
-                let field_name_string = field_name.to_string();
+    match derive_input {
+        DeriveInput {
+            ident: struct_name,
+            data: Data::Struct(
+                DataStruct {
+                    fields: Fields::Named(FieldsNamed { named: fields, .. }), ..
+                }
+            ), ..
+        } => {
+            let debug_struct_fields: proc_macro2::TokenStream = fields.iter().map(|field| {
+                if let Field { ident: Some(field_name), attrs, .. } = &field {
+                    let field_name_string = field_name.to_string();
 
-                let custom_format = match custom_format_from_debug_attribute(attrs) {
-                    Ok(custom_format) => custom_format,
-                    Err(error) => {
-                        return error.to_compile_error().into();
+                    let custom_format = match custom_format_from_debug_attribute(attrs) {
+                        Ok(custom_format) => custom_format,
+                        Err(error) => {
+                            return error.to_compile_error().into();
+                        }
+                    };
+
+                    let format = match custom_format {
+                        Some(custom_format) => quote! { &format_args!(#custom_format, &self.#field_name) },
+                        None => quote! { &self.#field_name },
+                    };
+
+                    quote! {
+                        .field(#field_name_string, #format)
                     }
-                };
-
-                let format = match custom_format {
-                    Some(custom_format) => quote! { &format_args!(#custom_format, &self.#field_name) },
-                    None => quote! { &self.#field_name },
-                };
-
-                quote! {
-                    .field(#field_name_string, #format)
+                } else {
+                    quote! {}
                 }
-            } else {
-                quote! {}
-            }
-        }).collect();
+            }).collect();
 
-        let struct_name_string = struct_name.to_string();
+            let struct_name_string = struct_name.to_string();
 
-        TokenStream::from(quote! {
-            impl std::fmt::Debug for #struct_name {
-                fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                    fmt.debug_struct(#struct_name_string)
-                        #debug_struct_fields
-                        .finish()
+            TokenStream::from(quote! {
+                impl std::fmt::Debug for #struct_name {
+                    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                        fmt.debug_struct(#struct_name_string)
+                            #debug_struct_fields
+                            .finish()
+                    }
                 }
-            }
-        })
-    } else {
-        TokenStream::new()
+            })
+        },
+        _ => TokenStream::new(),
     }
 }
