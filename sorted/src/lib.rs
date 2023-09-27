c67990c66389b57b4abf64ca2f1707c25dd732fa
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2023-09-27 16:46:32 -0700

    Refactor to use 'syn::parse' instead of 'parse_macro_input!'

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,22 +1,20 @@
 use proc_macro::TokenStream;
 use proc_macro2::Span;
 use syn::{
-    parse_macro_input,
     Item::{self, Enum},
 };
 
 #[proc_macro_attribute]
 pub fn sorted(_args: TokenStream, input: TokenStream) -> TokenStream {
-    let input_clone = input.clone();
-    let item: Item = parse_macro_input!(input_clone);
-
-    match sorted_impl(input, item) {
+    match sorted_impl(input) {
         Ok(output) => output,
         Err(err) => err.to_compile_error().into()
     }
 }
 
-fn sorted_impl(input: TokenStream, item: Item) -> syn::Result<TokenStream> {
+fn sorted_impl(input: TokenStream) -> syn::Result<TokenStream> {
+    let item: Item = syn::parse(input.clone())?;
+
     if let Enum(_) = item {
         Ok(input)
     } else {
