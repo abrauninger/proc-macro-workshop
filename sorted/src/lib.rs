953799c271fcdd3541369afa5238a5deb5c85ceb
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2023-09-27 09:43:31 -0700

    sorted: Implement step 01

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,9 +1,14 @@
 use proc_macro::TokenStream;
+use syn::{
+    parse_macro_input,
+    Item,
+};
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
-    let _ = input;
 
-    unimplemented!()
+    let input_clone = input.clone();
+    let _item: Item = parse_macro_input!(input_clone);
+    input
 }
