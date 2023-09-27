a3c8d2d7731b46be38299d449e1ec64152f1c32b
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2023-09-27 16:26:01 -0700

    Implement step 02

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,14 +1,21 @@
 use proc_macro::TokenStream;
+use quote::quote;
 use syn::{
     parse_macro_input,
-    Item,
+    Item::{self, Enum},
 };
 
 #[proc_macro_attribute]
-pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
-    let _ = args;
-
+pub fn sorted(_args: TokenStream, input: TokenStream) -> TokenStream {
     let input_clone = input.clone();
-    let _item: Item = parse_macro_input!(input_clone);
-    input
+    let item: Item = parse_macro_input!(input_clone);
+
+    if let Enum(_) = item {
+        input
+    } else {
+        quote! {
+            // We emit 'compile_error' directly so that the span is the proc-macro usage site itself
+            compile_error!("expected enum or match expression");
+        }.into()
+    }
 }
