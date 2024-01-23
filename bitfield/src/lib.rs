3415be407ebf679d69fd3679ccc155e8a3286aa2
Andy Brauninger <43097655+abrauninger@users.noreply.github.com>
2024-01-23 12:53:21 -0800

    Extract bitfield/src/checks.rs

--- bitfield/src/lib.rs
+++ bitfield/src/lib.rs
@@ -1,3 +1,5 @@
+pub mod checks;
+
 // Crates that have the "proc-macro" crate type are only allowed to export
 // procedural macros. So we cannot have one crate that defines procedural macros
 // alongside other types of public APIs like traits and structs.
@@ -19,59 +21,6 @@ pub trait Specifier {
 }
 
 gen_bit_width_types!(1..=64);
-
-pub mod checks {
-    pub trait TotalSizeIsMultipleOfEightBits {}
-
-    pub struct ZeroMod8 {}
-    pub struct OneMod8 {}
-    pub struct TwoMod8 {}
-    pub struct ThreeMod8 {}
-    pub struct FourMod8 {}
-    pub struct FiveMod8 {}
-    pub struct SixMod8 {}
-    pub struct SevenMod8 {}
-
-    impl TotalSizeIsMultipleOfEightBits for ZeroMod8 {}
-
-    pub trait Mod8Check {
-        type Type;
-    }
-
-    pub struct Mod8<const MOD8: usize> {}
-
-    impl Mod8Check for Mod8<0> {
-        type Type = ZeroMod8;
-    }
-
-    impl Mod8Check for Mod8<1> {
-        type Type = OneMod8;
-    }
-
-    impl Mod8Check for Mod8<2> {
-        type Type = TwoMod8;
-    }
-
-    impl Mod8Check for Mod8<3> {
-        type Type = ThreeMod8;
-    }
-
-    impl Mod8Check for Mod8<4> {
-        type Type = FourMod8;
-    }
-
-    impl Mod8Check for Mod8<5> {
-        type Type = FiveMod8;
-    }
-
-    impl Mod8Check for Mod8<6> {
-        type Type = SixMod8;
-    }
-
-    impl Mod8Check for Mod8<7> {
-        type Type = SevenMod8;
-    }
-}
 
 // Helper functions
 // TODO: Move these to a detail module?
